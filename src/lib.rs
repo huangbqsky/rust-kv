013d67ba480d44e892476147482818f6f1eeae84
huangbqsky <huangbqsky@gmail.com>
2023-03-20 19:18:01 +0800

    KV引擎engine模块

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,13 @@
 /*!
 The KvStore store key/value pairs.
  */
-mod command;
+mod engine;
 mod errors;
 mod proto;
-mod kv;
 mod server;
-mod kv_engine;
-mod sled;
 
-pub use command::Command;
+pub use engine::Command;
+pub use engine::{KvStore, KvsEngine, SledKvsEngine};
 pub use errors::{KVStoreError, Result};
 pub use proto::{Request, Response};
-pub use kv::KvStore;
-pub use kv_engine::KvsEngine;
-pub use crate::sled::SledKvsEngine;
 pub use server::{EngineType, KvServer};
